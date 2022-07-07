21ac579be90662b6b8a11812532ec302f14ceb5b
Fashid <fashid@hasnatech.onmicrosoft.com>
2022-07-07 14:50:37 +0530

    added

--- src/app/pages/m3-pop-up/m3-pop-up.component.ts
+++ src/app/pages/m3-pop-up/m3-pop-up.component.ts
@@ -12,4 +12,108 @@ export class M3PopUpComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  selected = -1
+
+  show(n:any){
+    this.selected = n;
+  }
+
+  close(n:any){
+    this.selected = -1;
+  }
+
+  content=[
+    {
+      img:"assets/m3_img5.jpg",
+      text:"Nutrient concept that improves Young Pig growability",
+      Popup:`<h2>Improves Young Pig growability*</h2>
+      <p>What does it do?</p>
+      <ul>
+        <li>Helps feed intake, weight gain, and growth uniformity</li>
+        <li>Improves gut health and reduces diarrhea</li>
+        <li>Stimulates development of the digestive system as piglets move away from milk</li>
+        <li>Reduces reliance on antibiotics and zinc oxide</li>
+      </ul>
+      <p>How does it do it?</p>
+      <ul>
+        <li>Prepares piglet digestive system for weaning by helping transition from sow milk to 100% solid feed</li>
+        <li>Splits transition feeding into phases to maximize feed intake and encourage growth 	Phases include Neonatal, Transition, NeoTrans, and Starter</li>
+        <li>Decreases feed costs by creating feeding system phases</li>
+      </ul>`
+      
+    },
+    {
+      img:"assets/m3_img6.jpg",
+      text:"Nutrient concept that enhances Sow productivity & Piglet livability",
+      Popup:`<h2>Enhances Sow productivity & Piglet livability</h2>
+      <p>What does it do?</p>
+      <ul>
+        <li>Increases milk production and sow immunity</li>
+        <li>Better manages hyper prolific sows</li>
+        <li>Develops sows/gilts and increases lifetime productivity</li>
+        <li>Reduces scouring and health issues </li>
+      </ul>
+      <p>How does it do it?</p>
+      <ul>
+        <li>Uses precise nutrition based on ingredient assessment to Improve product consistency</li>
+        <li>Uses SW DCAD calculation to Increase milk yield</li>
+        <li>New lactating sow diet created with Sow 3.0 offerings which improves body condition of sow and piglet weaning performance.</li>
+      </ul>`
+    },
+    {
+      img:"assets/m3_img7.jpg",
+      text:"Nutrient concept that increases Grower-Finisher profitability",
+      Popup:`<h2>Increases Grower-Finisher profitability</h2>
+      <p>What does it do?</p>
+      <ul>
+        <li>Helps reduce formulation costs by better utilizing nutrients</li>
+        <li>Improves carcass meat quality</li>
+        <li>Simplifies farm management and helps customers operate more efficiently </li>
+        <li>Increases gut health</li>
+      </ul>
+      <p>How does it do it?</p>
+      <ul>
+        <li>Compares common nutrients to reduce formulation costs by improving nutrient utilization using CNS nutrients</li>
+        <li>Estimates performance and feed conversion using pork MAX before customer decides</li>
+        <li>Compares suppliers so customers choose the one that best meets their needs</li>
+      </ul>`
+    },
+    {
+      img:"assets/m3_img8.png",
+      text:"Software platform that optimizes nutrition & modeling",
+      Popup:`<h2>Optimizes nutrition & modeling</h2>
+      <p>What does it do?</p>
+      <ul>
+        <li>Software platform that helps customers and their consultant make the best decisions in diet formulation, profitability, production, and nutrition </li>
+        <li>Provides solutions for changing markets and gives “what-if” scenarios that show economic projections</li>
+        <li>Determines a customized animal feed</li>
+        <li>Increases gut health</li>
+      </ul>
+      <p>How does it do it?</p>
+      <ul>
+        <li>Incorporates CNS technology and aligns it to formulation capabilities with the specific needs of the customer
+        </li>
+        <li>Uses CNS to understand animal requirements and links to the nutrient supply from key ingredients</li>
+      </ul>`
+    },
+    {
+      img:"assets/m3_img9.png",
+      text:"Software platform that creates efficiencies in the ingredient and nutrient supply chain",
+      Popup:`<h2>Efficiency in the ingredient and nutrient supply chain</h2>
+      <p>What does it do?</p>
+      <ul>
+        <li>Cloud-based nutrition platform that connects processes to create efficiencies </li>
+        <li>Optimizes nutrition by helping customers find the best suppliers and manage operations</li>
+        <li>Supports better animal performance, integrates decision-making, and improves operational excellence</li>
+      </ul>
+      <p>How does it do it?</p>
+      <ul>
+        <li>Manages ingredient and finished good samples through Nutrient Analysis capability using CNS</li>
+        <li>Connects to CNS data to compare suppliers and manage ingredient variability so customers make informed purchase decisions</li>
+        <li>Monitors feed bins, tracks feed consumption and feed bin history</li>
+        <li>Feed Ordering capability ensures customer can order the right feed at the right time</li>
+      </ul>`
+    },
+  ]
+
 }
